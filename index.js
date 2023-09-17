1d232358edefc669bebd2f6b71cd432c3243d8a1
jialei.jiang <jialei.jiang@designorder.cn>
2023-09-18 00:00:53 +0800

    fix

--- index.js
+++ index.js
@@ -17,15 +17,29 @@ if (major < 18) {
 }
 
 const spawn = require('cross-spawn')
+const path = require('path')
 
 
 
 const fs = require('fs-extra')
-console.log('fuck')
 
-const templateDir = path.join('', 'template');
+const templateDir = path.resolve(__dirname, 'template')
 if (fs.existsSync(templateDir)) {
     fs.copySync(templateDir, './');
+    fs.renameSync(path.resolve('gitignore'), '.gitignore')
 }
 
-spawn.sync('pnpm insall')
+spawn.sync('npm', ['install', '-g', 'pnpm'], { stdio: 'inherit' })
+spawn.sync('git', ['init'], { stdio: 'inherit' })
+
+const packagesJson = require(path.resolve(templateDir, 'package.json'))
+const rootDependencies = Object.keys(packagesJson.dependencies).map(key => `${key}@${packagesJson.dependencies[key]}`)
+spawn.sync('pnpm', ['install', '-w', ...rootDependencies], { stdio: 'inherit' })
+
+
+const repos = fs.readdirSync(path.resolve(templateDir, 'packages'))
+repos.forEach((repo) => {
+    const packagesJson = require(path.resolve(templateDir, 'packages', repo, 'package.json'))
+    const dependencies = Object.keys(packagesJson.dependencies).map(key => `${key}@${packagesJson.dependencies[key]}`)
+    spawn.sync('pnpm', ['install', ...dependencies], { stdio: 'inherit' })
+})
